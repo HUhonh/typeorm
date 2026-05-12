c5ccf32bdd127707f5a3e15af1b29111ed50673d
klor <133237397+HUhonh@users.noreply.github.com>
2026-05-13 00:21:11 +0800

    fix(mysql): handle length change in ALTER path

--- src/driver/mysql/MysqlQueryRunner.ts
+++ src/driver/mysql/MysqlQueryRunner.ts
@@ -1318,7 +1318,8 @@ export class MysqlQueryRunner extends BaseQueryRunner implements QueryRunner {
                 oldColumn.name = newColumn.name
             }
 
-            if (this.isColumnChanged(oldColumn, newColumn, true, true)) {
+            if (this.isColumnChanged(oldColumn, newColumn, true, true) ||
+            oldColumn.length !== newColumn.length) {
                 upQueries.push(
                     new Query(
                         `ALTER TABLE ${this.escapePath(table)} CHANGE \`${
